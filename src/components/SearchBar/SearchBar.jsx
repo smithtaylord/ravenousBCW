b22a6cd71872c14ddc8de2b5d88f9054aeee407b
Taylor Smith <smith.taylord@gmail.com>
2023-04-12 18:47:38 -0600

    SearchBar part way done setp 33 part one

---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './searchBar.css'
+
+const sortByOptions = {
+    'Best Match': 'best_match',
+    'Highest Rated': 'rating',
+    'Most Reviewed': 'review_count'
+}
+
+class SearchBar extends React.Component {
+    renderSortOptions() {
+
+    }
+
+}
