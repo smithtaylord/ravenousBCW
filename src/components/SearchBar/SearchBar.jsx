c57ab01f16616575b37100bcece87dbdfdcf2074
Taylor Smith <smith.taylord@gmail.com>
2023-04-13 07:14:53 -0600

    Part one Done

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -9,7 +9,32 @@ const sortByOptions = {
 
 class SearchBar extends React.Component {
     renderSortOptions() {
-
+        return Object.keys(sortByOptions).map(sbo => {
+            let sortByOptionsValue = sortByOptions[sbo]
+            return (
+                <li key={sortByOptionsValue}>{sbo}</li>
+            )
+        })
     }
 
+    render() {
+        return (
+            <div className="SearchBar">
+                <div className="SearchBar-sort-options">
+                    <ul>
+                        {this.renderSortOptions()}
+                    </ul>
+                </div>
+                <div className="SearchBar-fields">
+                    <input placeholder="Search Businesses" />
+                    <input placeholder="Where?" />
+                </div>
+                <div className="SearchBar-submit">
+                    <a>Let`&apos;`s Go</a>
+                </div>
+            </div>
+        )
+    }
 }
+
+export default SearchBar
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,23 +1,40 @@
 import React, { useState } from "react";
+import '../assets/scss/pages/HomePage.scss';
+import BusinessList from '../components/BusinessList/BusinessList.jsx';
+import SearchBar from '../components/SearchBar/SearchBar.jsx';
 
-export default function HomePage() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="home-page">
-      <div className="container my-3">
-        <div className="row">
-          <div className="col-4">
-            <div className="card">
-              <div className="card-body">
-                <button className="btn btn-success my-1" onClick={() => setCount((count) => count + 1)}>
-                  count is {count}
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
+class HomePage extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>ravenous</h1>
+        <SearchBar />
+        <BusinessList />
       </div>
-    </div>
-  )
+    )
+  }
 }
+
+export default HomePage
+
+// export default function HomePage() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <div className="home-page">
+//       <div className="container my-3">
+//         <div className="row">
+//           <div className="col-4">
+//             <div className="card">
+//               <div className="card-body">
+//                 <button className="btn btn-success my-1" onClick={() => setCount((count) => count + 1)}>
+//                   count is {count}
+//                 </button>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
