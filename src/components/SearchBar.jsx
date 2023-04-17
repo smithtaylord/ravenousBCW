0aeba631fa11a401c545f26778a3aaaed7edc685
Taylor Smith <smith.taylord@gmail.com>
2023-04-17 08:00:02 -0600

    Sets active search

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,16 +1,29 @@
 import React from "react";
 import '../assets/scss/components/SearchBar.scss'
+import { useState } from "react";
+
+export default function SearchBar() {
+const [sortBy, setSortBy] = useState('best_match');
+
+function handleSort(category){
+    setSortBy(category)
+}
 
 
-class SearchBar extends React.Component {
-    render() {
-        return (
+    return (
+        <div className="componentName">
             <div className="SearchBar">
                 <div className="SearchBar-sort-options">
                     <ul>
-                        <li className="px-5">Best Match</li>
-                        <li className="px-5">Highest Rated</li>
-                        <li className="px-5">Most Reviewed</li>
+                        <li onClick={() => handleSort('best_match')} 
+                        className={sortBy == 'best_match' ? 'px-5 active' : 'px-5'}>
+                            Best Match</li>
+                        <li onClick={() => handleSort('rating')} 
+                        className={sortBy == 'rating' ? 'px-5 active' : 'px-5'}>
+                            Highest Rated</li>
+                        <li onClick={() => handleSort('review_count')} 
+                        className={sortBy == 'review_count' ? 'px-5 active' : 'px-5'}>
+                            Most Reviewed</li>
                     </ul>
                 </div>
                 <div className="SearchBar-fields">
@@ -21,8 +34,17 @@ class SearchBar extends React.Component {
                     <a>Let's Go</a>
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
+
 }
 
-export default SearchBar
+// class SearchBar extends React.Component {
+//     render() {
+//         return (
+            
+//         )
+//     }
+// }
+
+// export default SearchBar
